ba7f2da252b6712f23c4c3c489b0816a780e0cfc
PupiTooo <pupe.dupe@gmail.com>
2018-07-23 15:56:08 +0200

    fix: return parsed response

--- src/Provider/Intercom.php
+++ src/Provider/Intercom.php
@@ -107,7 +107,7 @@ class Intercom extends AbstractProvider
 
         $request = $this->getAuthenticatedRequest(self::METHOD_GET, $url, $token);
 
-        return $this->getResponse($request);
+        return $this->getParsedResponse($request);
     }
 
     /**
